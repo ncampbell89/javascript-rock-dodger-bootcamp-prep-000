88c99384058769a8d77d509884420eca0606c1af
Nicole Campbell <nscampbell303@gmail.com>
2017-10-22 00:56:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -155,16 +155,6 @@ function positionToInteger(p) {
   return parseInt(p.split('px')[0]) || 0
 }
 
-function start() {
-  window.addEventListener('keydown', moveDodger)
-
-  START.style.display = 'none'
-
-  gameInterval = setInterval(function() {
-    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  }, 1000)
-}
-
 /**
  * End the game by clearing `gameInterval`,
  * removing all ROCKS from the DOM,
@@ -182,3 +172,15 @@ function endGame() {
 
    //alert("YOU LOSE!")
 }
+
+function start() {
+  window.addEventListener('keydown', moveDodger)
+
+  START.style.display = 'none'
+
+  gameInterval = setInterval(function() {
+    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
+  }, 1000)
+}
+
+
